9ba9a803c530b1f033fe2b63d2ce9e88dd8b23d0
Prashant Jha <prashantjha9549@gmail.com>
2026-02-23 13:30:37 +0530

    23/02/2026

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,9 +3,29 @@ import './App.css'
 function App() {
   const Carousal = lazy(() => import('../components/Carousal'))
   const Toast = lazy(() => import('../components/Toast'))
+  const Accordian = lazy(() => import('../components/Accordian'))
+
+  const accordianComps = [{
+        key:0,
+        title: 'First',
+        component: <p>First Component</p>
+    }, {
+        key:1,
+        title: 'Second',
+        component: <p>Second Component</p>
+    }, {
+        key:2,
+        title: 'Third',
+        component: <p>Third Component</p>
+    }, {
+        key:3,
+        title: 'Fourth',
+        component: <p>Fourth Component</p>
+    }]
   return (
     <Suspense fallback={<p>loading</p>} >
-      <Toast message={'alert'} duration={5000} position={'bottom'} type={'success'} />
+      {/* <Toast message={'alert'} duration={5000} position={'bottom'} type={'success'} /> */}
+      <Accordian accordianComps={accordianComps}/>
     </Suspense>
   )
 }
