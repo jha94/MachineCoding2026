14f44e480e65ff75e08ad5150c75d7c05064d3dc
Prashant Jha <prashantjha9549@gmail.com>
2026-03-06 13:07:52 +0530

    master:- todo list

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,29 +5,30 @@ function App() {
   const Toast = lazy(() => import('../components/Toast'))
   const Accordian = lazy(() => import('../components/Accordian'))
   const NestedComment = lazy(() => import('../components/NestedComment'))
+  const ToDoList = lazy(() => import('../components/ToDoList'))
 
   const accordianComps = [{
-        key:0,
-        title: 'First',
-        component: <p>First Component</p>
-    }, {
-        key:1,
-        title: 'Second',
-        component: <p>Second Component</p>
-    }, {
-        key:2,
-        title: 'Third',
-        component: <p>Third Component</p>
-    }, {
-        key:3,
-        title: 'Fourth',
-        component: <p>Fourth Component</p>
-    }]
+    key: 0,
+    title: 'First',
+    component: <p>First Component</p>
+  }, {
+    key: 1,
+    title: 'Second',
+    component: <p>Second Component</p>
+  }, {
+    key: 2,
+    title: 'Third',
+    component: <p>Third Component</p>
+  }, {
+    key: 3,
+    title: 'Fourth',
+    component: <p>Fourth Component</p>
+  }]
   return (
     <Suspense fallback={<p>loading</p>} >
       {/* <Toast message={'alert'} duration={5000} position={'bottom'} type={'success'} /> */}
       {/* <Accordian accordianComps={accordianComps}/> */}
-      <NestedComment/>
+      <ToDoList />
     </Suspense>
   )
 }
