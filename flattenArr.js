59a69a3083ffb6513e3956c463c944dbb383f2cb
Prashant Jha <prashantjha9549@gmail.com>
2026-03-16 12:46:24 +0530

    master:- arra flat done

--- flattenArr.js
+++ flattenArr.js
@@ -1,31 +1,16 @@
+const flatArr = (arr, depth) => {
+  const res = [];
+  const flat = (arr, initialDepth) => {
+    arr.forEach((value) => {
+      if (Array.isArray(value) && depth > initialDepth) {
+        flat(value, initialDepth + 1);
+      } else {
+        res.push(value);
+      }
+    });
+  };
+  flat(arr, 0);
+  return res;
+};
 
-const flatArr = (arr, level = Infinity) => {
-    let res = [];
-    // let depth = 0
-    // const flatten = (arr) => {
-    //     arr.forEach((value) => {
-    //         if (Array.isArray(value) && depth < level) {
-    //             depth += 1
-    //             flatten(value)
-    //         } else {
-    //             res.push(value)
-    //             depth = 0
-    //         }
-    //     })
-    // }
-    // flatten(arr)
-
-    const flattenArr = (arr, depth) => {
-        for(let value of arr){
-            if(Array.isArray(value) && depth<level){
-                flattenArr(value, depth+1)
-            } else{
-                res.push(value)
-            }
-        }
-    }
-    flattenArr(arr, 0)
-    return res
-}
-
-console.log(flatArr([1, [2], 3, [[4, 6], 5]], 3))
+console.log(flatArr([1, [2], 3, [[4, 6], 5]], 1));
